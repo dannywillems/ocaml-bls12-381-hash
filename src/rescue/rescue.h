173538cca8beb36f228ad5978d8c8b12283ab0de
Danny Willems <be.danny.willems@gmail.com>
2022-09-26 14:33:05 +0200

    Rescue: implement common interface
    Workflow
    - allocate_ctxt
    - set_state
    - apply_permutation
    - get_state
    
    + parameters

--- src/rescue/rescue.h
+++ src/rescue/rescue.h
@@ -4,20 +4,23 @@
 #include "blst.h"
 #include "blst_misc.h"
 
-#define WIDTH 3
-#define NB_ROUNDS 14
-#define NB_CONSTANTS (NB_ROUNDS * WIDTH * 2)
-
 typedef struct rescue_ctxt_s {
-  blst_fr s[WIDTH];
-  int i_round_key;
+  // Containts the state, the MDS and the constants
+  blst_fr *state;
+  int nb_rounds;
+  int state_size;
 } rescue_ctxt_t;
 
-size_t rescue_ctxt_sizeof();
+void marvellous_apply_permutation(rescue_ctxt_t *ctxt);
 
-int rescue_constants_init(blst_fr *akr, blst_fr **mds, int ark_len,
-                          int mds_nb_rows, int mds_nb_cols);
-void rescue_init(rescue_ctxt_t *ctxt, blst_fr *a, blst_fr *b, blst_fr *c);
-void marvellous_apply_perm(rescue_ctxt_t *ctxt);
-void rescue_get_state(blst_fr *a, blst_fr *b, blst_fr *c, rescue_ctxt_t *ctxt);
+blst_fr *rescue_get_state_from_context(rescue_ctxt_t *ctxt);
+
+blst_fr *rescue_get_mds_from_context(rescue_ctxt_t *ctxt);
+
+blst_fr *rescue_get_round_constants_from_context(rescue_ctxt_t *ctxt);
+
+int rescue_get_state_size_from_context(rescue_ctxt_t *ctxt);
+
+int rescue_get_number_of_constants_from_context(rescue_ctxt_t *ctxt);
+
 #endif
