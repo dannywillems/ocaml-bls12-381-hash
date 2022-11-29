989767ad8bf3241977685f94d56bedb9558e6abb
Danny Willems <be.danny.willems@gmail.com>
2022-11-29 14:33:46 +0100

    Poseidon: fix bench with new interface

--- benches/bench_poseidon.ml
+++ benches/bench_poseidon.ml
@@ -1,48 +1,43 @@
 open Core_bench
 
 let t1 =
+  let open Bls12_381_hash.Poseidon.Parameters in
   let name = "Benchmark one permutation of Poseidon128" in
-  let state_size, nb_full_rounds, nb_partial_rounds, batch_size, ark, mds =
-    Bls12_381_hash.Poseidon.Parameters.state_size_128_3
+  let state =
+    Array.init security_128_state_size_3.state_size (fun _ ->
+        Bls12_381.Fr.random ())
   in
-  let state = Array.init state_size (fun _ -> Bls12_381.Fr.random ()) in
-  let ctxt =
-    Bls12_381_hash.Poseidon.allocate_ctxt
-      state_size
-      nb_full_rounds
-      nb_partial_rounds
-      batch_size
-      ark
-      mds
-  in
+  let ctxt = Bls12_381_hash.Poseidon.allocate_ctxt security_128_state_size_3 in
   let () = Bls12_381_hash.Poseidon.set_state ctxt state in
   Bench.Test.create ~name (fun () ->
       Bls12_381_hash.Poseidon.apply_permutation ctxt)
 
-let create_bench nb_full_rounds nb_partial_rounds width batch_size =
-  let ark_length = width * (nb_full_rounds + nb_partial_rounds) in
+let create_bench nb_of_full_rounds nb_of_partial_rounds state_size batch_size =
+  let open Bls12_381_hash.Poseidon.Parameters in
+  let ark_length = state_size * (nb_of_full_rounds + nb_of_partial_rounds) in
   let ark = Array.init ark_length (fun _ -> Bls12_381.Fr.random ()) in
   let mds =
-    Array.init width (fun _ ->
-        Array.init width (fun _ -> Bls12_381.Fr.random ()))
+    Array.init state_size (fun _ ->
+        Array.init state_size (fun _ -> Bls12_381.Fr.random ()))
   in
-  let state = Array.init width (fun _ -> Bls12_381.Fr.random ()) in
-  let ctxt =
-    Bls12_381_hash.Poseidon.allocate_ctxt
-      width
-      nb_full_rounds
-      nb_partial_rounds
-      batch_size
-      ark
-      mds
+  let parameters =
+    { state_size;
+      batch_size;
+      round_constants = ark;
+      linear_layer = mds;
+      nb_of_full_rounds;
+      nb_of_partial_rounds
+    }
   in
+  let state = Array.init state_size (fun _ -> Bls12_381.Fr.random ()) in
+  let ctxt = Bls12_381_hash.Poseidon.allocate_ctxt parameters in
   let () = Bls12_381_hash.Poseidon.set_state ctxt state in
   let name =
     Printf.sprintf
       "Benchmark Poseidon: width = %d, partial = %d, full = %d, batch size = %d"
-      width
-      nb_partial_rounds
-      nb_full_rounds
+      state_size
+      nb_of_partial_rounds
+      nb_of_full_rounds
       batch_size
   in
   let t =
@@ -52,26 +47,28 @@ let create_bench nb_full_rounds nb_partial_rounds width batch_size =
   t
 
 let bench_neptunus =
-  let width = 5 in
-  let nb_full_rounds = 60 in
-  let nb_partial_rounds = 0 in
+  let state_size = 5 in
+  let nb_of_full_rounds = 60 in
+  let nb_of_partial_rounds = 0 in
   let batch_size = 1 in
-  let ark_length = width * (nb_full_rounds + nb_partial_rounds) in
+  let ark_length = state_size * (nb_of_full_rounds + nb_of_partial_rounds) in
   let ark = Array.init ark_length (fun _ -> Bls12_381.Fr.random ()) in
   let mds =
-    Array.init width (fun _ ->
-        Array.init width (fun _ -> Bls12_381.Fr.random ()))
+    Array.init state_size (fun _ ->
+        Array.init state_size (fun _ -> Bls12_381.Fr.random ()))
   in
-  let state = Array.init width (fun _ -> Bls12_381.Fr.random ()) in
-  let ctxt =
-    Bls12_381_hash.Poseidon.allocate_ctxt
-      width
-      nb_full_rounds
-      nb_partial_rounds
-      batch_size
-      ark
-      mds
+  let state = Array.init state_size (fun _ -> Bls12_381.Fr.random ()) in
+  let open Bls12_381_hash.Poseidon.Parameters in
+  let parameters =
+    { state_size;
+      batch_size;
+      nb_of_full_rounds;
+      nb_of_partial_rounds;
+      round_constants = ark;
+      linear_layer = mds
+    }
   in
+  let ctxt = Bls12_381_hash.Poseidon.allocate_ctxt parameters in
   let () = Bls12_381_hash.Poseidon.set_state ctxt state in
   let name = "Benchmark Neptunus" in
   Bench.Test.create ~name (fun () ->
